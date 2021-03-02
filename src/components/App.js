4740d8b68e48591e1eb94f98edf2f6f98a5301f1
Oscar Ore <oscar.ore2011@gmail.com>
2021-03-02 12:09:56 -0500

    Dashboardlist renders to page

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import DashboardList from "./DashboardList";
 
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
     return(
       <div className="App">
      <h2>Hello Youtube</h2>
+     <DashboardList/>
      
     </div>
     );
---
+++ src/components/DashboardList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DashboardList = () => {
+    return(
+        <h3>List!!!</h3>
+    )
+}
+
+export default DashboardList;
