22753df63ddbcc8fe9316e600f8a7272f312582d
Oscar Ore <oscar.ore2011@gmail.com>
2021-03-02 22:18:14 -0500

    added drag and drop feature to card components

--- src/components/DashboardList.js
+++ src/components/DashboardList.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import DashboardCard from './DashboardCard';
 import DashboardActionButton from './DashboardActionButton';
-
+import { Droppable } from "react-beautiful-dnd";
+ 
 const DashboardList = ({title, cards, listID }) => {
     return(
-        <div style={styles.containers}>
-            <h4>{title}</h4>
-            {cards.map(card => (
-            <DashboardCard key={card.id} text={card.text} />
+        <Droppable droppableId={String(listID)}>
+            {provided => (
+                 <div {...provided.droppableProps} ref={provided.innerRef} style={styles.container}>
+                 <h4>{title}</h4>
+                 {cards.map((card, index) => (
+                 <DashboardCard key={card.id} index={index} text={card.text} id={card.id} />
             ))}
             <DashboardActionButton listID={listID} />
+            {provided.placeholder}
         </div>
+            )}
+    </Droppable>
     );  
 };
 
