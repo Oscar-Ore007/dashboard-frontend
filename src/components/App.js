3c5fafeb510c0eb8d17aff37763c508fa6527cb1
Oscar Ore <oscar.ore2011@gmail.com>
2021-03-02 22:00:21 -0500

    added the drag and drop package

--- src/components/App.js
+++ src/components/App.js
@@ -2,22 +2,32 @@ import React, { Component } from "react";
 import DashboardList from "./DashboardList";
 import { connect } from "react-redux";
 import DashboardActionButton from "./DashboardActionButton";
+import { DragDropContext } from "react-beautiful-dnd";
 
 class App extends Component {
+
+  onDragEnd = () => {
+    //reorderinglofic 
+  };
   render() {
 
     const {lists} = this.props;
     return(
+      <DragDropContext onDragEnd={this.onDragEnd}>
       <div className="App">
      <h2>Hello Youtube</h2>
      <div style={styles.listsContainer}>
       { lists.map(list => (
-      <DashboardList listID={list.id}
-      key={list.id} title ={list.title} cards={list.cards} />
+      <DashboardList 
+      listID={list.id}
+      key={list.id} 
+      title ={list.title} 
+      cards={list.cards} />
       ))}
       <DashboardActionButton list /> 
      </div>
     </div>
+    </DragDropContext>
     );
   }
 }
