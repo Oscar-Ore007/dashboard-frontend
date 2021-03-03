1e294e04e2ce88a9e7e12bac2a9bbaa14aee4a13
Oscar Ore <oscar.ore2011@gmail.com>
2021-03-02 23:55:49 -0500

    added the ability to drag and drop from list to list

--- src/components/DashboardCard.js
+++ src/components/DashboardCard.js
@@ -3,6 +3,11 @@ import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import CardContent from '@material-ui/core/CardContent';
 import { Draggable } from "react-beautiful-dnd";
+import styled from "styled-components";
+
+const CardContainer = styled.div`
+margin-bottom: 8px;
+`
 
 
 
@@ -10,13 +15,15 @@ const DashBoardCard = ({text, id, index }) => {
     return (
     <Draggable draggableId={String(id)} index={index}>
         {provided => (
-            <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-             <Card style={styles.cardContainer}>
+            <CardContainer ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+            
+             <Card>
              <CardContent>
                 <Typography gutterBottom>{text}</Typography>
             </CardContent>
             </Card>
-        </div>
+    
+        </CardContainer>
         )}
       </Draggable>
     );
--- src/reducers/listsReducer.js
+++ src/reducers/listsReducer.js
@@ -87,8 +87,24 @@ const listsReducer = (state = initialState, action) => {
                     //in the same list 
                     if(droppableIdStart === droppableIdEnd) {
                         const list = state.find(list => droppableIdStart === list.id);
-                        const card = list.cards.splice(droppableIndexStart, 1)
-                        list.cards.splice(droppableIndexEnd, 0, ...card)
+                        const card = list.cards.splice(droppableIndexStart, 1);
+                        list.cards.splice(droppableIndexEnd, 0, ...card);
+                    }
+
+
+
+                    //other list 
+                    if(droppableIdStart !== droppableIdEnd) {
+                        //find the list where the drag happened 
+                        const listStart = state.find(list => droppableIdStart === list.id)
+
+                        //pull out the card from this list 
+                        const card = listStart.cards.splice(droppableIndexStart, 1);
+                        // find the list where drag ended 
+                        const listEnd = state.find(list => droppableIdEnd === list.id);
+
+                        // put the card in the new list 
+                        listEnd.cards.splice(droppableIndexEnd, 0, ...card)
                     }
                     return newState;
 
