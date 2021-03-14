df796ff601190b8852ebec3ddd55b0ed3b14235e
Oscar Ore <oscar.ore2011@gmail.com>
2021-03-14 12:50:27 -0400

    added DashboardCreate to Dashbaordlist

--- src/components/DashboardList.js
+++ src/components/DashboardList.js
@@ -16,25 +16,112 @@ height: 100%;
 margin: 0 8px 0 0;
 `;
 
+const StyledInput = styled.input`
+width: 100%;
+border: none;
+outline-color: blue;
+border-radius: 3px;
+margin-bottom: 3px;
+padding: 5px;
+
+
+`;
+
+const TitleContainer = styled.div`
+width: 100%;
+height; 100%;
+display: flex;
+justify-content: space-between;
+align-items: center;
+cursor: pointer;
+`;
+
+const DeleteButton = styled(Icon)`
+cursor: pointer;
+transition: opacity 0.3s ease-in-out;
+opacity: 0.4;
+&hover {
+    opacity: 0.8;
+}
+`;
+
+const ListTitle = styled.h4`
+transition: background 0.3s ease-in;
+${TitleContainer}:hover & {
+    background: #ccc;
+}
+`;
 
  
-const DashboardList = ({title, cards, listID, index }) => {
+const DashboardList = ({title, cards, listID, index, dispatch }) => {
+    const [isEditing, setIsEditing] = useState(false);
+    const [listTitle, setListTitle] = useState(title);
+
+    const renderEditInput = () => {
+        return (
+            <form onSubmit={handleFinishEditing}>
+                <StyledInput
+                type="text"
+                value={listTitle}
+                onChange={handleChange}
+                autoFocus
+                onFocus={handleFocus}
+                onBlur={handleFinishEditing}
+                />
+                </form>
+        );
+    };
+
+    const handleFocus = e => {
+        e.preventDefault();
+        setListTitle(e.target.value);
+    };
+
+    const handleFinishedEditing = e => {
+        setIsEditing(false);
+        dispatch(editTitle(listID, listTitle));
+};
+
+    const handleDeleteList = () => {
+        dispatch(deleteList(listID));
+    };
+
     return(
         <Draggable  draggableId={String(listID)} index={index}>
             {provided => (
         <ListContainer 
             {...provided.draggableProps} 
+            {...provided.dragHandleProps}
             ref={provided.innerRef} 
-            {...provided.dragHandleProps}>
-            <Droppable droppableId={String(listID)}>
+            >
+            <Droppable droppableId={String(listID)} type="card">
                    {provided => (
+                       <div>
+                           <div>
+                               {isEditing ? (
+                                   renderEditInput()
+                               ) : (
+                                <TitleContainer onClick={() => setIsEditing(true)}>
+                                <ListTitle>{listTitle}</ListTitle>
+                                <DeleteButton onClick={handleDeleteList}>
+                                    delete
+                                    </DeleteButton>
+                                    </TitleContainer>
+                               )}
+                               </div>
                        <div {...provided.droppableProps} ref= {provided.innerRef}> 
-                        <h4>{title}</h4>
                         {cards.map((card, index) => (
-                        <DashboardCard key={card.id} index={index} text={card.text} id={card.id} />
+                        <DashboardCard 
+                        key={card.id} 
+                        text={card.text}
+                        id={card.id}
+                        index={index} 
+                        listID={listID}
+                        />
                    ))}
-                   <DashboardActionButton listID={listID} />
                    {provided.placeholder}
+                   <Dashboardreate listID={listID} />
+                   </div>
                    </div>
                    )}
            </Droppable>
@@ -44,16 +131,4 @@ const DashboardList = ({title, cards, listID, index }) => {
     );  
 };
 
-const styles = { 
-    container: {
-        backgroundColor: "#dfe3e6",
-        borderRadius: 3,
-        width: 300,
-        padding: 8,
-        height: "100%",
-        marginRight: 8
-    }
-    
-};
-
-export default DashboardList;
+export default connect ()(DashboardList);
