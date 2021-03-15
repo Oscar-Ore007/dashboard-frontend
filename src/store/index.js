e93bb2be4365ab9c5328f66cf965e8ab0144b08b
Oscar Ore <oscar.ore2011@gmail.com>
2021-03-15 00:58:22 -0400

    cleaned up index.js in store folder

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,19 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from "../reducers";
+import thunk from "redux-thunk";
 
-const reducer = () => {};
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-const store = createStore(rootReducer)
+const persistConfig = {
+    key: "root",
+    storage
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export default () => {
+    let store = createStore(persistedReducer, applyMiddleware(thunk));
+    let persistor = persistStore(store);
+    return { store, persistor };
+};
