23083787612582149c9c971b80e2541c2c33de1a
Oscar Ore <oscar.ore2011@gmail.com>
2021-03-16 15:08:55 -0400

    added create project div

--- src/containers/ProjectContainer.js
+++ src/containers/ProjectContainer.js
@@ -1,13 +1,29 @@
-import React, { Component } from 'react' 
+import React, { Component } from 'react';
+import Project from "../components/Project";
 
 class ProjectContainer extends Component {
+
+    state = {
+        projectList: [
+            { name: 'Project1'}
+        ]
+    };
+
+    handleClick = () => {
+        this.setState({
+            projectList: [{ name: "New Project" }, ...this.state.projectList]
+        });
+    };
     render() {
         return (
             <div className="project-container">
+                <div>
                 <h2> Project Container </h2>
+                </div>
+                    <Project projects={this.state.projectList} handleClick={this.handleClick} />
             </div>
-        )
+        );
     }
 }
 
-export default ProjectContainer
+export default ProjectContainer;
