3e40a9177ec9c8c717a5bd5a25e733e970eadaa3
Oscar Ore <oscar.ore2011@gmail.com>
2021-03-17 09:09:03 -0400

    added a delete button and more css

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Card extends Component {
+
+    state = {
+        hover: false
+    };
+
+    handleMouseOver = () => {
+        this.setState({
+            hover: true 
+        });
+    };
+
+    handleMouseOver = () => {
+        this.setState({
+            hover: false
+        });
+    };
     render() {
         return (
-            <div>
-                <div className="to-do-card"><h2>{this.props.task}</h2></div>
+                <div className='card-component'>
+                    <div
+                    onMouseEnter={this.handleMouseOver}
+                    onMouseLeave={this.handleMouseOver}
+                    className='to-do-card'
+                    >
+                        <h2>{this.props.task}</h2>
+                        {this.state.hover ? <h3 onClick={this.handleClick}>x</h3> : null}
+                    </div>
             </div>
-        )
+        );
     }
 }
 
-export default Card
+export default Card;
--- src/containers/ListContainer.js
+++ src/containers/ListContainer.js
@@ -4,15 +4,47 @@ import ListCards from './ListCards'
 class ListsContainer extends Component {
 
     state = {
-        listCards: this.props.currentProject.lists
+        listCards: this.props.currentProject.lists,
+        inputValue: '',
+        clicked: false 
     }
 
-    // handleClick = () => {
-    //     this.setState({
-    //         listCards: [ {name: 'New Task'}, 
-    //         ...this,state.listCards]
-    //     })
-    // }
+    handleClick = () => {
+        this.setState({
+           clicked: !this.state.clicked 
+        })
+    }
+
+    handleChange = () => {
+        this.setState({
+           inputValue: e.target.value 
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.props)
+        fetch(`http://localhost:3000/lists`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+                Authorization: localStorage.token
+            },
+            body: JSON.stringify({
+                project_id: 2,
+                name: this.state.inputValue,
+            })
+        })
+        .then(resp => resp.json())
+        .then(respData => {
+            this.setState({
+                listCards: [respData.data.attributes, ...this.state.listCards],
+                clicked: false,
+                inputValue: ''
+            })
+        })
+    }
 
 
     render() {
@@ -20,9 +52,20 @@ class ListsContainer extends Component {
             <div className="list-container">
                 <ListCards listCards={this.state.listCards} loadCurrentProject=
                 {this.props.loadCurrentProject}/>
+                { !this.state.clicked ?
                 <div onClick={this.handleClick} className='list-card-container'>
                     <h3>Add a List</h3>
                 </div>
+                :
+                <form onSubmit={this.handleSubmit} className='list-container-form'>
+                    <input type="text" onChange={this.handleChange} value=
+                    {this.state.inputValue} placeholder="Enter a List title..." /> 
+                    <div className='list-container-form-submit-wrapper'>
+                        <input type="submit" />
+                        <h2 onClick={this.handleClick}>X</h2>
+            </div>
+            </form>
+            }
             </div>
         )
     }
